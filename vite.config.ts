a1dcac58a2b544c0847a6ad02d85a9c6b9d3c15d
playorhire <playorhire@gmail.com>
2026-04-18 18:03:25 +0500

    Added vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@
 //     componentTagger (dev-only), VITE_* env injection, @ path alias, React/TanStack dedupe,
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+// import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
@@ -12,4 +14,11 @@ export default defineConfig({
     allowedHosts: ["localhost", "exposure-undergo-tubular.ngrok-free.dev"],
     port: 8080
   }
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+
 });
+
